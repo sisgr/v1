c61fe6bbbf606194ffab5de53089d024d189f9fc
alessandro <alessandro@alessandro-Aspire-5750>
2013-10-15 23:27:57 -0300

    Configuração inicial do tiles2 com bootstrap. Funcionamento do "ALOMUNDO" da aplicação

---
+++ sis-gr/src/br/com/sisgr/controller/OlaMundoController.java
@@ -0,0 +1,18 @@
+package br.com.sisgr.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class OlaMundoController {
+
+	@RequestMapping("/ola")
+	public ModelAndView olaMundo() {
+		ModelAndView resultado = new ModelAndView("mundo");
+		resultado.addObject("data", new Date());
+		return resultado;
+	}
+}
